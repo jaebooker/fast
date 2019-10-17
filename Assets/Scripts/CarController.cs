b2e6afad3162b3d87e8e6c37c0fda848d7af8b62
jaebooker <jaesonbooker@gmail.com>
2019-10-17 15:43:38 -0700

    added physics and scripts

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,77 @@
+﻿//using system.collections;
+//using system.collections.generic;
+//using unityengine;
+
+//public class CarController : monobehaviour
+//{
+//    public void getinput()
+//    {
+//        m_horizontalinput = input.getaxis("horizontal");
+//        m_verticalinput = input.getaxis("vertical");
+//    }
+
+//    private void steer()
+//    {
+//        m_steerangle = maxsteerangle * m_horizontalinput;
+//        frontdriverw.steerangle = m_steerangle;
+//        frontpassengerw.steerangle = m_steerangle;
+
+//    }
+
+//    private void accelerate()
+//    {
+//        frontdriverw.motortorque = m_verticalinput * motorforce;
+//        frontpassengerw.motortorque = m_verticalinput * motorforce;
+//    }
+
+//    private void updatewheelposes()
+//    {
+//        updatewheelpose(frontdriverw, frontdrivert);
+//        updatewheelpose(frontpassengerw, frontpassengert);
+//        updatewheelpose(backdriverw, backdrivert);
+//        updatewheelpose(backpassengerw, backpassengert);
+//    }
+
+//    private void updatewheelpose(wheelcollider _collider, transform _transform)
+//    {
+//        vector3 _pos = _transform.position;
+//        quaternion _quat = _transform.rotation;
+
+//        _collider.getworldpose(out _pos, out _quat);
+
+//        _transform.position = _pos;
+//        _transform.rotation = _pos;
+//    }
+
+//    private void fixedupdate()
+//    {
+//        getinput();
+//        steer();
+//        accelerate();
+//        updatewheelposes();
+
+//    }
+
+//    private float m_horizontalinput;
+//    private float m_verticalinput;
+//    private float m_steerangle;
+
+//    public wheelcollider frontdriverw, frontpassengerw;
+//    public wheelcollider backdriverw, backpassengerw;
+//    public transform frontdrivert, frontpassengert;
+//    public transform backdrivert, backpassengert;
+//    public float maxsteerangle = 30;
+//    public float motorforce = 50;
+
+//    //start is called before the first frame update
+//   void start()
+//    {
+
+//    }
+
+//    //update is called once per frame
+//  void update()
+//    {
+
+//    }
+//}
---
+++ Assets/Scripts/Car_Control.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Control : MonoBehaviour
+{
+    public float MotorForce, SteerForce, BrakeForce;
+    public WheelCollider FR_L_Wheel, FR_R_Wheel, RE_L_Wheel, RE_R_Wheel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v;
+        float h;
+
+        if (OVRInput.Get(OVRInput.Button.One)==true)
+        {
+            v = MotorForce;
+        }
+        else
+        {
+            v = 0;
+        }
+
+        if (OVRInput.Get(OVRInput.Button.Two)==true)
+        {
+            h = SteerForce;
+        }
+        else
+        {
+            h = 0;
+        }
+
+        RE_L_Wheel.motorTorque = v;
+        RE_R_Wheel.motorTorque = v;
+
+        FR_L_Wheel.steerAngle = h;
+        FR_R_Wheel.steerAngle = h;
+
+
+        if (OVRInput.Get(OVRInput.Button.Three) == true)
+        {
+            RE_L_Wheel.brakeTorque = BrakeForce;
+            RE_R_Wheel.brakeTorque = BrakeForce;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            RE_L_Wheel.brakeTorque = 0;
+            RE_R_Wheel.brakeTorque = 0;
+        }
+        if (OVRInput.Get(OVRInput.Button.One)==false)
+        {
+            RE_L_Wheel.brakeTorque = BrakeForce;
+            RE_R_Wheel.brakeTorque = BrakeForce;
+        }
+        else
+        {
+            RE_L_Wheel.brakeTorque = 0;
+            RE_R_Wheel.brakeTorque = 0;
+        }
+    }
+}
