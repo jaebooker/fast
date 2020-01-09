9e0f01e0d3780cc188e404c7e9512c80690f133e
jaebooker <jaesonbooker@gmail.com>
2020-01-09 15:45:55 -0800

    added scripts and other things

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void raceTrackOne()
+    {
+        SceneManager.LoadScene("scene2-night");
+    }
+    public void raceTrackOneSpaceship()
+    {
+        SceneManager.LoadScene("scene1-night");
+    }
+    public void back()
+    {
+        SceneManager.LoadScene("menu");
+    }
+    public void endGame()
+    {
+        Application.Quit();
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
---
+++ Assets/Scripts/WellCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WellCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision objectInfo)
+    {
+        if (objectInfo.collider.tag == "car1")
+        {
+            SceneManager.LoadScene("scene2-night");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
